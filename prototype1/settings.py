51d1f1c2d0fac1b513c9541f6a0911e374c5549b
peddiashrith <44424610+peddiashrith@users.noreply.github.com>
2020-06-09 20:32:23 +0530

    staticfiles

--- prototype1/settings.py
+++ prototype1/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'home'
